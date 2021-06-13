d082f0f05ea5a0d3385ea91c34d92007c9c9de11
thouseef46 <85782042+thouseef46@users.noreply.github.com>
2021-06-13 16:07:11 +0530

    Add files via upload

---
+++ days2.py
@@ -0,0 +1,62 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #days-2 enlist bootcamp
+>>> #printing a value:
+>>> print("30 days 30 hour challenge")
+30 days 30 hour challenge
+>>> #assiging string to variable:
+>>> hours='thirty'
+>>> print(hours)
+thirty
+>>> #string indexing
+>>> 
+>>> days='thirty days'
+>>> print(days[0])
+t
+>>> challenge='I WILL WIN'
+>>> print(challenge[7:10])
+WIN
+>>> #length of character:
+>>> print(len(challenge))
+10
+>>> #lower character:
+>>> print(challenge.lower())
+i will win
+>>> 
+>>> #string concatenation:
+>>> a="30 days"
+>>> b="30 hours"
+>>> c=a+b
+>>> print(c)
+30 days30 hours
+>>> 
+>>> #adding space during concatenation:
+>>> d=a+""+b
+>>> c=a+""+b
+>>> print(c)
+30 days30 hours
+>>> 
+>>> #casefold:
+>>> 
+>>> text='thirty days and thirty hours'
+>>> x= text.casefold()
+>>> print(x)
+thirty days and thirty hours
+>>> 
+>>> #----------
+>>> 
+>>> 
+>>> sampletext="DON'T TROUBLE TROUBLE UNTIL TROUBLE TROUBLES YOU."
+>>> y=sampletext.capitalize()
+>>> print(y)
+Don't trouble trouble until trouble troubles you.
+>>> y=sampletext.find('0')
+>>> print(y)
+-1
+>>> y=sampletext.isalpha()
+>>> print(y)
+False
+>>> y=sampletext.isalnum()
+>>> print(y)
+False
+>>> 
