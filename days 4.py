196c68f087d4abb572d510850af628369b9078ab
thouseef46 <85782042+thouseef46@users.noreply.github.com>
2021-06-15 11:52:35 +0530

    Add files via upload

---
+++ days 4.py
@@ -0,0 +1,15 @@
+a=b=c=100
+print("divide a by 10",a/10)
+print("multiply b by 50",b*50)
+print("add c value by 60",c+60)
+#------
+string_var="abcdef"
+print(string_var)
+replacedstring=string_var.replace('d','G')
+print(replacedstring)
+
+#------
+a=10
+b=10.5
+print("convert a to float=",float(a))
+print("convert b to int=",int(b))
