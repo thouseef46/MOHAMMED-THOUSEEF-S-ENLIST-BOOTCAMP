56a7027af9296823aa5f374fcd21fe436721d6b8
thouseef46 <85782042+thouseef46@users.noreply.github.com>
2021-06-23 21:17:22 +0530

    Add files via upload

---
+++ Days10.py
@@ -0,0 +1,7 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+import pandas as pd
+cars = {"Names": ["AUDI","BENZ","BMW"],"Price": [12000,36000,45000]}
+var = pd.DataFrame(cars)
+print(var)
+print("*"*25)
